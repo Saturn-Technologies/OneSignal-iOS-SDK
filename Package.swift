e95f15090ed13b3139ec73b6ea03ac6b18543edc
DJ Tarazona <me@djtarazona.com>
2023-09-02 18:54:44 -0400

    Remove binary targets

--- Package.swift
+++ Package.swift
@@ -40,26 +40,6 @@ let package = Package(
                 "OneSignalCore"
             ],
             path: "OneSignalOutcomesWrapper"
-        ),
-        .binaryTarget(
-          name: "OneSignal",
-          url: "https://github.com/OneSignal/OneSignal-iOS-SDK/releases/download/3.12.6/OneSignal.xcframework.zip",
-          checksum: "48d79753e67f2ad802e1cc738dbdce1a2d24517f9812e67d76c2387965bc9c5d"
-        ),
-        .binaryTarget(
-          name: "OneSignalExtension",
-          url: "https://github.com/OneSignal/OneSignal-iOS-SDK/releases/download/3.12.6/OneSignalExtension.xcframework.zip",
-          checksum: "22d050a8e2d41dd7cea7331bf98363ff264c38175128df0f14a3c7d86c8156ae"
-        ),
-        .binaryTarget(
-          name: "OneSignalOutcomes",
-          url: "https://github.com/OneSignal/OneSignal-iOS-SDK/releases/download/3.12.6/OneSignalOutcomes.xcframework.zip",
-          checksum: "4d6e08640dc49986424caa0163afa022faa4020dc4e642812fc9a04d57dca12b"
-        ),
-        .binaryTarget(
-          name: "OneSignalCore",
-          url: "https://github.com/OneSignal/OneSignal-iOS-SDK/releases/download/3.12.6/OneSignalCore.xcframework.zip",
-          checksum: "0b9ac1abd8fb242be1597939fbd3d6b807bad15253c22bea05936789cd0fbd6b"
         )
     ]
 )
